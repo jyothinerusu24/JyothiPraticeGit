2ec69916f006eb474f14dd5ac2d1732901978a98
Amrutha <amruthathatikonda2002@gmail.com>
2025-03-20 12:03:06 +0530

    Amrutha branch created

---
+++ src/test/java/MySeleniumPrograms/MySeleniumPrograms/Amrutha_gitbranch.java
@@ -0,0 +1,10 @@
+package MySeleniumPrograms.MySeleniumPrograms;
+
+public class Amrutha_gitbranch {
+
+	public static void main(String[] args) {
+		System.out.println("Creating Amrutha Branch");
+
+	}
+
+}
